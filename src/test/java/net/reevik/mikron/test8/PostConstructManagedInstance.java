642f18ee1ddb608bcb7c6a5489eef38fc5fcaa0c
Erhan Bagdemir <erhan@reevik.net>
2024-02-10 10:14:01 +0100

    PostConstruct life-cycle

---
+++ src/test/java/net/reevik/mikron/test8/PostConstructManagedInstance.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024 Erhan Bagdemir. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.reevik.mikron.test8;
+
+import net.reevik.mikron.annotation.Managed;
+import net.reevik.mikron.annotation.PostConstruct;
+
+@Managed
+public class PostConstructManagedInstance {
+
+  private int fieldUnderTest;
+
+  @PostConstruct
+  public void postConstruct() {
+    fieldUnderTest = 42;
+  }
+
+  public int getFieldUnderTest() {
+    return fieldUnderTest;
+  }
+}
